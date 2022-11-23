41a3ac4709a154c01894d37028049c3451006080
wagnerAJS221 <wagneralex221@gmai.com>
2022-11-23 15:36:51 +0000

    time to push

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-
-
-function Main({initialFormState, formState, setFormState}) {
+function Main({ initialFormState, formState, setFormState }) {
   //const [open, setOpen] = useState(false); //Ignore this state
 
   const handleSubmit = (event) => {
@@ -16,7 +14,7 @@ function Main({initialFormState, formState, setFormState}) {
     const targetName = event.target.name
     const targetType = event.target.type
     const targetChecked = event.target.value
-  
+
     if (targetName === 'name') {
       setFormState({ ...formState, fullName: targetValue })
     }
@@ -24,297 +22,197 @@ function Main({initialFormState, formState, setFormState}) {
 
   return (
     <main className="main">
-      <section className={`main__list ${open ? "open" : ""}`}>
+      <section className={`main__list ${open ? 'open' : ''}`}>
         <h2>Answers list</h2>
         {/* answers should go here */}
       </section>
 
       <section className="main__form">
         <form class="form">
-          
-        <h2>Tell us what you think about your rubber duck!</h2>
-        <div className="form__group">
-          <h3> What would you say are the best features of your rubber duck</h3>
-        <ul>
-            <li>
-              <label>
-                <input
-                  name="spend-time"
-                  type="checkbox"
-                  value="swimming"
-                /> It's Yellow
-              </label>
-              
-            </li>
-            <li>
-              <label>
-                
+          <h2>Tell us what you think about your rubber duck!</h2>
+          <div className="form__group">
+            <h3>
+              {' '}
+              What would you say are the best features of your rubber duck
+            </h3>
+            <ul>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="swimming" />{' '}
+                  It's Yellow
+                </label>
+              </li>
+              <li>
+                <label>
                   <input name="spend-time" type="checkbox" value="bathing" />
                   It's Squeaks
-              </label>
-              
-            </li>
-            <li>
-              <label>
-                
-                  <input
-                  name="spend-time"
-                  type="checkbox"
-                  value="chatting"
-                /> It has a logo.
-                
-              </label>
-              
-            </li>
-            <li>
-              <label>
-                <input name="spend-time" type="checkbox" value="noTime" />
-                It is big
-              </label>
-              
-            </li>
-          </ul>
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="chatting" />{' '}
+                  It has a logo.
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="noTime" />
+                  It is big
+                </label>
+              </li>
+            </ul>
 
-          <h3> What would you say are the worst features of your rubber duck</h3>
-        <ul>
-            <li>
-              <label>
-                <input
-                  name="spend-time"
-                  type="checkbox"
-                  value="swimming"
-                /> It's Yellow
-              </label>
-              
-            </li>
-            <li>
-              <label>
-                
+            <h3>
+              {' '}
+              What would you say are the worst features of your rubber duck
+            </h3>
+            <ul>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="swimming" />{' '}
+                  It's Yellow
+                </label>
+              </li>
+              <li>
+                <label>
                   <input name="spend-time" type="checkbox" value="bathing" />
                   It's Squeaks
-              </label>
-              
-            </li>
-            <li>
-              <label>
-                
-                  <input
-                  name="spend-time"
-                  type="checkbox"
-                  value="chatting"
-                /> It has a logo.
-                
-              </label>
-              
-            </li>
-            <li>
-              <label>
-                <input name="spend-time" type="checkbox" value="noTime" />
-                It is big
-              </label>
-              
-            </li>
-          </ul>
-        </div>
-        <div class="form__group radio">
-          
-          <h3>How do you rate your rubber duck consistency?</h3>
-          {/* <!-- Radio inputs go here --> */}
-          <ul>
-            <li>
-              <input id="color-one" type="radio" name="color" value="1" />
-              <label
-                for="color-one">
-                1
-              </label>
-              
-            </li>
-            <li>
-              <input id="color-two" type="radio" name="color" value="2" />
-              <label
-                for="color-two" >
-                2
-
-              </label>
-              
-            </li>
-            <li>
-              <input id="color-three" type="radio" name="color" value="3" />
-              <label
-                for="color-three">
-                3
-
                 </label>
-            
-            </li>
-            <li>
-              <input id="color-four" type="radio" name="color" value="4" />
-              
-              <label
-                for="color-four">
-                4
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="chatting" />{' '}
+                  It has a logo.
                 </label>
-
-            </li>
-          </ul>  
-
-
-          <h3>How do you rate your rubber duck colour?</h3>
-          {/* <!-- Radio inputs go here --> */}
-          <ul>
-            <li>
-              <input id="color-one" type="radio" name="color" value="1" />
-              <label
-                for="color-one">
-                1
-              </label>
-              
-            </li>
-            <li>
-              <input id="color-two" type="radio" name="color" value="2" />
-              <label
-                for="color-two" >
-                2
-
-              </label>
-              
-            </li>
-            <li>
-              <input id="color-three" type="radio" name="color" value="3" />
-              <label
-                for="color-three">
-                3
-
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="noTime" />
+                  It is big
                 </label>
-            
-            </li>
-            <li>
-              <input id="color-four" type="radio" name="color" value="4" />
-              
-              <label
-                for="color-four">
-                4
+              </li>
+            </ul>
+          </div>
+          <div class="form__group radio">
+            <h3>How do you rate your rubber duck consistency?</h3>
+            {/* <!-- Radio inputs go here --> */}
+            <ul>
+              <li>
+                <input id="color-one" type="radio" name="color" value="1" />
+                <label for="color-one">1</label>
+              </li>
+              <li>
+                <input id="color-two" type="radio" name="color" value="2" />
+                <label for="color-two">2</label>
+              </li>
+              <li>
+                <input id="color-three" type="radio" name="color" value="3" />
+                <label for="color-three">3</label>
+              </li>
+              <li>
+                <input id="color-four" type="radio" name="color" value="4" />
+
+                <label for="color-four">4</label>
+              </li>
+            </ul>
+
+            <h3>How do you rate your rubber duck colour?</h3>
+            {/* <!-- Radio inputs go here --> */}
+            <ul>
+              <li>
+                <input id="color-one" type="radio" name="color" value="1" />
+                <label for="color-one">1</label>
+              </li>
+              <li>
+                <input id="color-two" type="radio" name="color" value="2" />
+                <label for="color-two">2</label>
+              </li>
+              <li>
+                <input id="color-three" type="radio" name="color" value="3" />
+                <label for="color-three">3</label>
+              </li>
+              <li>
+                <input id="color-four" type="radio" name="color" value="4" />
+
+                <label for="color-four">4</label>
+              </li>
+            </ul>
+
+            <h3>How do you rate your rubber duck logo?</h3>
+            {/* <!-- Radio inputs go here --> */}
+            <ul>
+              <li>
+                <input id="color-one" type="radio" name="color" value="1" />
+                <label for="color-one">1</label>
+              </li>
+              <li>
+                <input id="color-two" type="radio" name="color" value="2" />
+                <label for="color-two">2</label>
+              </li>
+              <li>
+                <input id="color-three" type="radio" name="color" value="3" />
+                <label for="color-three">3</label>
+              </li>
+              <li>
+                <input id="color-four" type="radio" name="color" value="4" />
+
+                <label for="color-four">4</label>
+              </li>
+            </ul>
+          </div>
+
+          <div class="form__group">
+            <h3>How do you like to spend time with your rubber duck</h3>
+            {/* <!-- checkboxes go here --> */}
+
+            <ul>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="swimming" />{' '}
+                  Swimming
                 </label>
-
-            </li>
-          </ul> 
-
-          <h3>How do you rate your rubber duck logo?</h3>
-          {/* <!-- Radio inputs go here --> */}
-          <ul>
-            <li>
-              <input id="color-one" type="radio" name="color" value="1" />
-              <label
-                for="color-one">
-                1
-              </label>
-              
-            </li>
-            <li>
-              <input id="color-two" type="radio" name="color" value="2" />
-              <label
-                for="color-two" >
-                2
-
-              </label>
-              
-            </li>
-            <li>
-              <input id="color-three" type="radio" name="color" value="3" />
-              <label
-                for="color-three">
-                3
-
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="bathing" />
+                  Bathing
                 </label>
-            
-            </li>
-            <li>
-              <input id="color-four" type="radio" name="color" value="4" />
-              
-              <label
-                for="color-four">
-                4
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="chatting" />
+                  Chatting
                 </label>
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="noTime" />I
+                  don't like to spend time with it
+                </label>
+              </li>
+            </ul>
+          </div>
 
-            </li>
-          </ul>       
-        </div>
+          <label>
+            What else have you got to say about your rubber duck?
+            <textarea name="review" cols="30" rows="10"></textarea>
+          </label>
 
-        <div class="form__group">
-          <h3>How do you like to spend time with your rubber duck</h3>
-          {/* <!-- checkboxes go here --> */}
+          <label>
+            Put your name here (if you feel like it):
+            <input type="text" name="username" value="" />
+          </label>
 
-          <ul>
-            <li>
-              <label>
-                <input
-                  name="spend-time"
-                  type="checkbox"
-                  value="swimming"
-                /> Swimming
-              </label>
-              
-            </li>
-            <li>
-              <label>
-                
-                  <input name="spend-time" type="checkbox" value="bathing" />Bathing
-              </label>
-              
-            </li>
-            <li>
-              <label>
-                
-                  <input
-                  name="spend-time"
-                  type="checkbox"
-                  value="chatting"
-                />Chatting
-                
-              </label>
-              
-            </li>
-            <li>
-              <label>
-                <input name="spend-time" type="checkbox" value="noTime" />I don't like to
-                spend time with it
-              </label>
-              
-            </li>
-          </ul>
+          <label>
+            Leave us your email pretty please??
+            <input type="email" name="email" value="" />
+          </label>
 
-
-        </div>
-        
-        <label>What else have you got to say about your rubber duck?<textarea
-            name="review"
-            cols="30"
-            rows="10" >
-          </textarea>
-        </label>
-
-        <label>
-          Put your name here (if you feel like it):
-          <input
-            type="text"
-            name="username"
-            value="" />
-        </label>
-
-        <label>
-          Leave us your email pretty please??
-          <input
-            type="email"
-            name="email"
-            value="" />
-            </label>
-
-        <input class="form__submit" type="submit" value="Submit Survey!" />
-
+          <input class="form__submit" type="submit" value="Submit Survey!" />
         </form>
-    </section>
+      </section>
     </main>
-  );
+  )
 }
 
-export default Main;
+export default Main
